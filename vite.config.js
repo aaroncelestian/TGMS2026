b4f9ec694e09bba73c85f66e928a55eb37bd99cb
Aaron Celestian <aaron.celestian@gmail.com>
2026-02-08 17:13:19 -0800

    Fix base path to match repository name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tgms2026-exhibit/',
+  base: '/TGMS2026/',
   build: {
     outDir: 'dist'
   }
